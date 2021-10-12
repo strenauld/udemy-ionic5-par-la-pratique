ac03e10d1507a2abfb1613a7439189cbc9bf8bd8
Stéphane RENAULD <stephane.renauld@kaizen-solutions.net>
2021-10-12 11:53:17 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic5_freezer_manager_udemy',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
