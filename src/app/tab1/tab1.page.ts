b93279c2aea00a2576e19e50c4e05810a8a30b66
Stephane Renauld <stephane.renauld1@gmail.com>
2021-10-12 14:02:01 +0200

    Section 2: 4 création d'un reactive form

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  form: FormGroup;
 
   constructor() {}
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      foodName: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      datePlacedInFreezer: new FormControl(null, {
+        validators: [Validators.required]
+      })
+    })
+  }
+
+  add(): void {
+    console.log(this.form)
+  }
 
 }
