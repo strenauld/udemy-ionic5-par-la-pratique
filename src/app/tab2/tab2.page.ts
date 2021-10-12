6a32d0d2ff3d8da7be4c0778f9357ce0beca5c30
Stephane Renauld <stephane.renauld1@gmail.com>
2021-10-12 15:34:02 +0200

    Section 3: 9 lister tous les éléments

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { FoodService } from './../services/food.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  allFoodInFreezer = [];
 
-  constructor() {}
+  constructor(private _foodService: FoodService) {}
+  
+  ngOnInit(): void {
+    this.allFoodInFreezer = this._foodService.allFood;
+    console.log('ngOnInit: ', this.allFoodInFreezer);
+  }
 
 }
