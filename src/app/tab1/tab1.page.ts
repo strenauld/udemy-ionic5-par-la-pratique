ec274f72fb3dc97cb916c769469d8b0c357987c5
Unknown <stephane.renauld1@gmail.com>
2021-10-15 16:12:34 +0200

    Section 6: 30 Menu déroulant permettant de sélectionner une catégorie

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { FoodService } from './../services/food.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import categories from "../shared/food-categories";
+import { Category } from '../interfaces/category.interface';
 
 @Component({
   selector: 'app-tab1',
@@ -10,13 +12,18 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class Tab1Page implements OnInit {
   form: FormGroup;
   isLoading = false;
+  allCategories: Category[] = [];
 
   constructor(private _foodService: FoodService) {}
 
   ngOnInit(): void {
+    this.allCategories = categories;
     this.form = new FormGroup({
       foodName: new FormControl(null, {
         validators: [Validators.required]
+      }),
+      category: new FormControl({
+        Validators: [Validators.required]
       }),
       datePlacedInFreezer: new FormControl(null, {
         validators: [Validators.required]
